674092ba0ccc6179275e33e1147c3405cca8fb0f
OscarNR <oscar.alonso23.34@gmail.com>
2025-04-15 09:27:25 -0600

    feat: implement full CRUD functionality for products and categories modules

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -3,7 +3,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from . import views
 
+app_name = 'products'
 
 urlpatterns = [
-    path('', views.ProductListView.as_view(), name='product_list')
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', views.ProductListView.as_view(), name='product_list'),
+    path('create/', views.ProductCreateView.as_view(), name='product_create'),
+    path('<slug:slug>/', views.ProductDetailView.as_view(), name='product_detail'),
+    path('<slug:slug>/update/', views.ProductUpdateView.as_view(), name='product_update'),
+    path('<slug:slug>/delete/', views.ProductDeleteView.as_view(), name='product_delete'),
+]
