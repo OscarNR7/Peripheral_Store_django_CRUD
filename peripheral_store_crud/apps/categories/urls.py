1a6189dcb30847f01ed9f217527b2f6505997d20
OscarAlonsoNavaRivera-214729 <al214729@alumnos.uacj.mx>
2025-04-16 21:18:49 -0600

    Updated user templates and restructured overall navigation

--- peripheral_store_crud/apps/categories/urls.py
+++ peripheral_store_crud/apps/categories/urls.py
@@ -7,8 +7,8 @@ app_name = 'categories'
 
 urlpatterns = [
     path('', views.CategoryListView.as_view(), name='category_list'),
-    path('create/', views.CategoryCreateView.as_view(), name='category_create'),
-    path('<int:pk>/', views.CategoryDetailView.as_view(), name='category_detail'),
-    path('<int:pk>/update/', views.CategoryUpdateView.as_view(), name='category_update'),
-    path('<int:pk>/delete/', views.CategoryDeleteView.as_view(), name='category_delete'),
+    path('/create-category/', views.CategoryCreateView.as_view(), name='category_create'),
+    path('/<int:pk>/', views.CategoryDetailView.as_view(), name='category_detail'),
+    path('/<int:pk>/update/', views.CategoryUpdateView.as_view(), name='category_update'),
+    path('/<int:pk>/delete/', views.CategoryDeleteView.as_view(), name='category_delete'),
 ]
--- peripheral_store_crud/apps/products/urls.py
+++ peripheral_store_crud/apps/products/urls.py
@@ -7,8 +7,8 @@ app_name = 'products'
 
 urlpatterns = [
     path('', views.ProductListView.as_view(), name='product_list'),
-    path('create/', views.ProductCreateView.as_view(), name='product_create'),
-    path('<slug:slug>/', views.ProductDetailView.as_view(), name='product_detail'),
-    path('<slug:slug>/update/', views.ProductUpdateView.as_view(), name='product_update'),
-    path('<slug:slug>/delete/', views.ProductDeleteView.as_view(), name='product_delete'),
+    path('/create-product/', views.ProductCreateView.as_view(), name='product_create'),
+    path('/<slug:slug>/', views.ProductDetailView.as_view(), name='product_detail'),
+    path('/<slug:slug>/update/', views.ProductUpdateView.as_view(), name='product_update'),
+    path('/<slug:slug>/delete/', views.ProductDeleteView.as_view(), name='product_delete'),
 ]
