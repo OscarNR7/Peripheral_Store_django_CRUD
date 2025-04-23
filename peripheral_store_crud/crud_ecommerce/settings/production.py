c2ff8d3ea6d4d6086e3f516e039d0a7dbbeecb4a
OscarAlonsoNavaRivera-214729 <al214729@alumnos.uacj.mx>
2025-04-23 11:34:25 -0600

    Fix: Configure Cloudinary integration in settings for image storage

--- peripheral_store_crud/crud_ecommerce/settings/production.py
+++ peripheral_store_crud/crud_ecommerce/settings/production.py
@@ -6,6 +6,9 @@ These settings are optimized for deployment on Render.
 import os
 import dj_database_url
 from .base import *
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -61,6 +64,13 @@ CLOUDINARY_STORAGE = {
 
 DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 
+cloudinary.config(
+    cloud_name=os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    api_key=os.environ.get('CLOUDINARY_API_KEY'),
+    api_secret=os.environ.get('CLOUDINARY_API_SECRET'),
+    secure=True
+)
+
 # Email configuration - Configure in environment variables
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.environ.get('EMAIL_HOST', '')
@@ -68,3 +78,9 @@ EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
+
+#DEBUG:
+print("Cloudinary config (prod):")
+print("CLOUD_NAME:", os.environ.get('CLOUDINARY_CLOUD_NAME'))
+print("API_KEY:", os.environ.get('CLOUDINARY_API_KEY'))
+print("API_SECRET:", os.environ.get('CLOUDINARY_API_SECRET'))
